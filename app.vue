1d8d87f6ecdcf00c83f4ad90f7392084829dedc3
Jeffrey Brignoli <jeffrey.brignoli@sky.ch>
2023-05-16 22:28:59 +0200

    add google forms

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <header>
+      Brasserie Brignoli
+    </header>
+    <section class="bg-slate-300 ">
+      <!-- contact formulaire-->
+      <!-- nom, message, email-->
+      <form id="formId" action="https://docs.google.com/forms/d/e/1FAIpQLSfZ3_-6S3V7FOHBpwI2AXCRzyNvHjBEA9tJ-qjJtJ0qP8bK-Q/formResponse">
+
+        <br>
+        <label for="entry.970021322">Nom</label>
+        <br>
+        <input type="text" name="entry.970021322" />
+        <br>
+        <label for="entry.177502483">Message</label>
+        <br>
+        <textarea id="entry.177502483"  rows="5" cols="33" name="entry.177502483" >
+          hello
+        </textarea>
+
+        <br>
+        <label for="entry.1934065761">Enter your globex.com email:</label>
+        <br>
+        <input type="email" id="entry.1934065761" size="10" required name="entry.1934065761">
+        <br>
+        <input type="submit" value="Submit" />
+      </form>
+    </section>
+
+    <section>
+     
+    </section>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./node_modules/flowbite.{js,ts}"
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+  ,
 }
 
