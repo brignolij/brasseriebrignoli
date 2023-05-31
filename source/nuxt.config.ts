36d06b8c9e1f62645097bd7963a05c4efccd254c
Jeffrey Brignoli <jeffrey.brignoli@sky.ch>
2023-05-31 08:56:39 +0200

    update config

--- source/nuxt.config.ts
+++ source/nuxt.config.ts
@@ -3,16 +3,22 @@ export default defineNuxtConfig({
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
+            tailwindcss: {},
+            autoprefixer: {},
         },
-      },
+    },
     modules: [
         //'@nuxtjs/tailwindcss',
         'nuxt-icon',
     ],
     app: {
         // baseURL: '/nuxt-github-pages/' // baseURL: '/<repository>/'
-    }
+    },
+    generate: {
+        nojekyll: true, //not working on this version    
+        fallback: '404.html',
+    },
+    ssr: false, //When false  
+    target: "static", // and static, nuxt generates a SPA
 
 })
