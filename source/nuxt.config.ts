74d87f4b38e328aed20c75d776131ee9c433079d
Jeffrey Brignoli <jeffrey.brignoli@sky.ch>
2023-05-31 08:36:26 +0200

    upgrade nuxt

--- source/nuxt.config.ts
+++ source/nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
     modules: [
-        '@nuxtjs/tailwindcss',
+        //'@nuxtjs/tailwindcss',
         'nuxt-icon',
     ],
     app: {
-        baseURL: '/nuxt-github-pages/' // baseURL: '/<repository>/'
-      }
-  
+        // baseURL: '/nuxt-github-pages/' // baseURL: '/<repository>/'
+    }
+
 })
