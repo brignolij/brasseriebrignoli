9cf37baf16d5b06c17a7f9f3185f7bacc555d73d
Jeffrey Brignoli <jeffrey.brignoli@sky.ch>
2023-07-25 21:48:07 +0200

    update size

--- components/sections/SectionBeers.vue
+++ components/sections/SectionBeers.vue
@@ -1,29 +1,32 @@
 <template>
     <!-- Section: Design Block -->
     <section class="mb-32 text-center text-gray-800 " id="beers">
-      <h1 class="text-3xl font-bold mb-12 text-yellowbeer font-barlow uppercase">Les Bières</h1>
+      <h1 class="text-5xl font-bold mb-12 text-yellowbeer font-barlow uppercase">Les Bières</h1>
   
       <div class="grid grid-cols-1  lg:grid-cols-3 gap-4 px-10">
 
-        <div class="block text-greenjeff">
-          <h3 class="text-3xl font-bold font-highvoltagerough">La Jeff</h3>
+        <div class="block ">
           <img class="mt-3 max-w-120 mx-auto" src="/images/beer_jeff.png" />
-          <p class="mt-3 ">IPA - 5.6%</p>
+          <h3 class="text-5xl font-bold font-highvoltagerough mt-4 text-greenjeff">La Jeff</h3>
+
+          <p class="mt-3">IPA - 5.6%</p>
           <p>Claire, légère, peu amère et fruitée </p>
         </div>
   
-        <div class="block text-blueczech">
-          <h3 class="text-3xl font-bold font-highvoltagerough">
-            La Czech <span class="font-barlow text-base">(la Tchèque)</span>
-          </h3>
+        <div class="block ">
+          
           <img class="mt-3 max-w-120 mx-auto" src="/images/beer_czech.png" />
+          <h3 class="text-5xl font-bold font-highvoltagerough mt-4 text-blueczech">
+            La Czech <span class="font-barlow text-base ">(la Tchèque)</span>
+          </h3>
           <p class="mt-3">Pils Lager - 6.2%</p>
           <p>Claire, maltée et légère amertume </p>
         </div>
   
-        <div class="block text-red1-700 text-redirish">
-          <h3 class="text-3xl font-bold font-highvoltagerough">L'Irish</h3>
+        <div class="block text-red1-700 ">
           <img class="mt-3 max-w-120 mx-auto" src="/images/beer_irish.png" />
+          <h3 class="text-5xl font-bold font-highvoltagerough mt-4 text-redirish">L'Irish</h3>
+
           <p class="mt-3">Irish Red Ale - 6.6%</p>
           <p>Rouge, notes de torréfaction, légère amertume et final fruité.</p>
         </div>
